c244b12494b0693b31874332e795f208fc425dab
Gabriel Davi <111718103+gsouzakkj@users.noreply.github.com>
2024-04-30 19:55:44 -0300

    .

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0){
         return [dias,horas,minutos,segundos];
     } else {
-        return [10,437,13420,23450];
+        return [340,437,13420,23450];
     }
 }
 
