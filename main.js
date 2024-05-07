7cb9acad84fa7c5c7c042d71a92cf1ee124e3e81
Gabriel Davi <111718103+gsouzakkj@users.noreply.github.com>
2024-05-07 19:09:13 -0300

    ,

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-10T20:20:55");
 const tempoObjetivo2 = new Date("2024-11-12T13:00:00");
 const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo4 = new Date("2024-06-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
